f0aca113e31f75f7756c09466bd16abe15c92e6d
pengc <pengcheng3211@gmail.com>
2017-06-26 14:04:11 +0800

    增加httpUtil工具类

---
+++ src/main/java/com/share/network/HttpUtil.java
@@ -0,0 +1,34 @@
+package com.share.network;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * HTTP工具类<br/>
+ * 对于各种HTTP请求操作处理
+ *
+ * @author pengc
+ * @see com.share.network
+ * @date 2017/6/26
+ */
+public class HttpUtil {
+
+    /** IE浏览器标识 **/
+    private static String[] IEBrowserSignals = {"MSIE", "Trident", "Edge"};
+
+    /**
+     * 判断是否为IE浏览器<br>
+     * 通过使用Trident和Edge关键字来判断是否是微软的浏览器
+     *
+     * @param request
+     * @return 返回true 为IE浏览器，否则为其他浏览器
+     * @date 2017/6/26
+     */
+    public static boolean isMSBrowser(HttpServletRequest request) {
+        String userAgent = request.getHeader("User-Agent");
+        for (String signal : IEBrowserSignals) {
+            if (userAgent.contains(signal))
+                return true;
+        }
+        return false;
+    }
+}
